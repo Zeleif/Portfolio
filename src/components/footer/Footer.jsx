4085b9a8bf34e413d390a8d7361c63e89262bcaa
SKR <128593450+Zeleif@users.noreply.github.com>
2024-03-24 18:52:46 +0100

    Update Footer.jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer>
       <p>&copy; 2023 Sylvie</p>
-      <a href="cv/cv_2024-03-21_Sylvie_KORVAL-ROBERT" download="cv.pdf">
+      <a href="cv/CV_2024-03-21_Sylvie_KORVAL-ROBERT.pdf" download="cv.pdf">
         <button>Télécharger mon CV</button>
       </a>
     </footer>
